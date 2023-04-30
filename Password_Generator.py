c1dc9b7f948ea64c70e8994ffa7f6584926bfa8d
silvercry007 <shubhamrules007@gmail.com>
2023-04-30 14:02:44 +0530

    Passowrd Generator to save passwords in a CSV file

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,5 +1,9 @@
 import random
 import string
+import csv
+
+
+
 def password_generator(password_length, use_lowercase, use_uppercase, use_digits, use_symbols):
     # Define the character sets for the password based on the input
     characters = ""
@@ -11,10 +15,10 @@ def password_generator(password_length, use_lowercase, use_uppercase, use_digits
         characters += string.digits
     if use_symbols:
         characters += string.punctuation
-    # Use random.choices() to generate a password of the desired length
+    # Using random.choices() to generate a password of the desired length
     password = "".join(random.choices(characters, k=password_length))
     return password
-# Example usage
+
 password_length = int(input("Specify the lenght of the password :- "))
 lowercase = str(input("Do you want to use lowercase? (T/F):- ").upper())
 if lowercase == "T":
@@ -38,3 +42,23 @@ else:
     use_symbols = False   
 password = password_generator(password_length, use_lowercase, use_uppercase, use_digits, use_symbols)
 print("Your password is:", password)
+
+title = input("Title of Password:>>")
+ 
+print("Title:" + title +"\nPassword:"+ password)
+
+# # create the csv writer to create the file with the headings (Comment out if you want to add the title once)
+# f = open('demo.csv', 'w')
+  
+
+# writer = csv.writer(f)
+# header = ['Title','Password']
+# writer.writerow(header)
+
+with open("passwords.csv", "a", encoding="utf-8", newline="") as file:
+    writer = csv.writer(file)
+        # Writes the title
+    writer.writerow([str(title),str(password)])
+
+
+
